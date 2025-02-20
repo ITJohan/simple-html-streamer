b8a385384bd77f80254af22847fc0e5ec07f3f8d
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-02-20 20:55:24 +0100

    refactor: rename to html

--- my-component.js
+++ my-component.js
@@ -1,18 +1,14 @@
 // @ts-check
 
-import { CustomElement } from "./custom-element.js";
-import { Computed, Signal } from "./signal.js";
+import { Computed, html, Signal } from "./html.js";
 
 class MyComponent extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
-    const customElement = new CustomElement();
 
     const test = new Signal(1);
-    const role = new Signal("heading");
     const toogle = new Signal(true);
-    // TODO: content is not showing
     const content = new Computed(
       () => {
         return toogle.value ? "<p>this is true</p>" : "<p>this is false</p>";
@@ -27,7 +23,7 @@ class MyComponent extends HTMLElement {
     const onClick = new Signal(() => test.value++);
     const title = new Computed(() => `hello ${test.value}`, [test]);
 
-    const html = customElement.createTemplate`
+    shadow.append(...html`
       <h1 test="${test}" role="heading">${title}</h1>
       <button onclick="${onClick}" title="test">Click here</button>
       <button onclick="${() =>
@@ -36,25 +32,9 @@ class MyComponent extends HTMLElement {
         <p>${content.value}</p>
       </div>
       <aside>
-        ${customElement.createTemplate`<p>this is a test</p>`}
+        ${html`<p>this is a test</p>`}
       </aside>
-    `;
-
-    shadow.append(...html);
-
-    /**
-     * IDEA: we switch the passed in values with ids, and just parse the html with attributes and everything.
-     *
-     * "@click" can be replaced with on:click or something.
-     */
-    // this.render(this.html`
-    //   <h1 test=${test} role=${role}>${title}</h1>
-    //   <button @click=${onClick} title="test">Click here</button>
-    //   ${content.value}
-    //   <ul>
-    //     ${items}
-    //   </ul>
-    // `);
+    `);
   }
 
   static observedAttributes = ["test"];
