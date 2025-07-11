d765031bfce29217b0c807f46abe47e9e15b1c7f
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-07-11 07:14:03 +0200

    refactor: extract suspend to function

--- sw.js
+++ sw.js
@@ -1,125 +1,90 @@
 import { html, stream, suspend } from "./html-streamer.js";
 
+/**
+ * @param {ReturnType<html>} placeholder
+ * @param {ReturnType<html>} loaded
+ * @param {ReturnType<html>} error
+ * @returns {ReturnType<suspend>}
+ */
+const delayedComponent = (placeholder, loaded, error) => {
+  return suspend(
+    placeholder,
+    new Promise((resolve, reject) => {
+      setTimeout(
+        () => Math.random() > 0.5 ? resolve(loaded) : reject(error),
+        Math.random() * 3000,
+      );
+    }),
+  );
+};
+
 self.addEventListener("fetch", (event) => {
   const fetchEvent = /** @type {FetchEvent} */ (event);
-  const url = new URL(fetchEvent.request.url);
 
-  if (url.pathname === "/" && fetchEvent.request.method === "GET") {
-    const generator = html`
-      <h1>This is the header loaded in the initial shell</h1>
-      ${suspend(
-        html`
-          <p>Loading #1 ⏳</p>
-        `,
-        new Promise((resolve) =>
-          setTimeout(() =>
-            resolve(html`
+  const generator = html`
+    <h1>This is the header loaded in the initial shell</h1>
+    ${delayedComponent(
+      html`
+        <p>Loading #1 ⏳</p>
+      `,
+      html`
+        <p>
+          Loaded #1 ✅ ${delayedComponent(
+            html`
+              <p>Loading nested ⏳</p>
+            `,
+            html`
               <p>
-                Loaded #1 ✅ ${suspend(
+                Loaded nested ✅ ${delayedComponent(
                   html`
-                    <span>Loading nested⏳</span>
+                    <p>Loading deeply nested ⏳</p>
                   `,
-                  new Promise((resolve) =>
-                    setTimeout(() =>
-                      resolve(html`
-                        <div>
-                          Loaded nested✅ ${suspend(
-                            html`
-                              <p>Loading deeply nested⏳</p>
-                            `,
-                            new Promise((resolve) =>
-                              setTimeout(() =>
-                                resolve(html`
-                                  <p>Loaded deeply nested✅</p>
-                                `), Math.random() * 1000)
-                            ),
-                          )}
-                        </div>
-                      `), Math.random() * 1000)
-                  ),
+                  html`
+                    <p>Loaded deeply nested ✅</p>
+                  `,
+                  html`
+                    <p>Error deeply nested ❌</p>
+                  `,
                 )}
               </p>
-            `), Math.floor(Math.random() * 3000))
-        ),
-      )} ${suspend(
-        html`
-          <p>Loading #2 ⏳</p>
-        `,
-        new Promise((resolve, reject) =>
-          setTimeout(() =>
-            Math.random() > 0.5
-              ? resolve(html`
-                <p>Loaded #2 ✅</p>
-              `)
-              : reject(html`
-                <p>Error #2 ❌</p>
-              `), Math.floor(Math.random() * 3000))
-        ),
-      )} ${suspend(
-        html`
-          <p>Loading #3 ⏳</p>
-        `,
-        new Promise((resolve, reject) =>
-          setTimeout(() =>
-            Math.random() > 0.5
-              ? resolve(html`
-                <p>Loaded #3 ✅</p>
-              `)
-              : reject(html`
-                <p>Error #3 ❌</p>
-              `), Math.floor(Math.random() * 3000))
-        ),
-      )} ${suspend(
-        html`
-          <p>Loading #4 ⏳</p>
-        `,
-        new Promise((resolve, reject) =>
-          setTimeout(() =>
-            Math.random() > 0.5
-              ? resolve(html`
-                <p>Loaded #4 ✅</p>
-              `)
-              : reject(html`
-                <p>Error #4 ❌</p>
-              `), Math.floor(Math.random() * 3000))
-        ),
-      )}${suspend(
-        html`
-          <p>Loading #5 ⏳</p>
-        `,
-        new Promise((resolve, reject) =>
-          setTimeout(() =>
-            Math.random() > 0.5
-              ? resolve(html`
-                <p>Loaded #5 ✅</p>
-              `)
-              : reject(html`
-                <p>Error #5 ❌</p>
-              `), Math.floor(Math.random() * 3000))
-        ),
-      )}${suspend(
-        html`
-          <p>Loading #6 ⏳</p>
-        `,
-        new Promise((resolve, reject) =>
-          setTimeout(() =>
-            Math.random() > 0.5
-              ? resolve(html`
-                <p>Loaded #6 ✅</p>
-              `)
-              : reject(html`
-                <p>Error #6 ❌</p>
-              `), Math.floor(Math.random() * 3000))
-        ),
-      )}
-      <footer>This is the footer loaded in the initial shell</footer>
-    `;
-    const htmlStream = stream(generator);
+            `,
+            html`
+              <p>Error nested ❌</p>
+            `,
+          )}
+        </p>
+      `,
+      html`
+        <p>Error #1 ❌</p>
+      `,
+    )} ${delayedComponent(
+      html`
+        <p>Loading #2 ⏳</p>
+      `,
+      html`
+        <p>Loaded #2 ✅</p>
+      `,
+      html`
+        <p>Error #2 ❌</p>
+      `,
+    )} ${delayedComponent(
+      html`
+        <p>Loading #3 ⏳</p>
+      `,
+      html`
+        <p>Loaded #3 ✅</p>
+      `,
+      html`
+        <p>Error #3 ❌</p>
+      `,
+    )}
+    <footer>This is the footer loaded in the initial shell</footer>
+  `;
+  const htmlStream = stream(generator);
 
-    fetchEvent.respondWith(
-      new Response(htmlStream, {
-        headers: { "Content-Type": "text/html; charset=utf-8" },
-      }),
-    );
-  }
+  fetchEvent.respondWith(
+    new Response(htmlStream, {
+      headers: { "Content-Type": "text/html; charset=utf-8" },
+    }),
+  );
 });
